be9fbe2d294add1bd75b0666561ade6666872243
DevJohann <felipepoveda273@gmail.com>
2024-05-11 17:18:22 -0500

    Initial commit

---
+++ src/main/java/co/rootsoftworks/demo/app/udemyinterceptoresag_1/Udemyinterceptoresag1Application.java
@@ -0,0 +1,13 @@
+package co.rootsoftworks.demo.app.udemyinterceptoresag_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Udemyinterceptoresag1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Udemyinterceptoresag1Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/rootsoftworks/demo/app/udemyinterceptoresag_1/Udemyinterceptoresag1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.rootsoftworks.demo.app.udemyinterceptoresag_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Udemyinterceptoresag1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
